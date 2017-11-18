5621b41b95813abb046c3ba10fd6518252ef03cd
扬 何 <1015485344@qq.com>
2017-11-18 22:33:10 +0800

    1

--- GGGL/Math/MathMacro.h
+++ GGGL/Math/MathMacro.h
@@ -1,6 +1,5 @@
 #ifndef _MATH_MACRO_H_
 #define _MATH_MACRO_H_
-#define M_PI 3.14159265358979323846
 
 #define RADIANS_TO_DEGREES(radians) ((radians) * (180.0 / M_PI))
 #define DEGREES_TO_RADIANS(angle) ((angle) / 180.0 * M_PI)
